7acf6acffed0e3a892ea73b45d92a340209daaca
gfieni <gfieni@users.noreply.github.com>
2016-11-04 12:36:33 +0100

    feat(remove-cluster): 'remove-cluster' command now kill the job before removing the nodes in libmachine storage.

--- command/remove_cluster.go
+++ command/remove_cluster.go
@@ -1,9 +1,14 @@
 package command
 
 import (
+	"encoding/json"
+
 	"github.com/docker/machine/commands/mcndirs"
 	"github.com/docker/machine/libmachine"
+	"github.com/docker/machine/libmachine/log"
 	"github.com/docker/machine/libmachine/persist"
+
+	"github.com/Spirals-Team/docker-machine-driver-g5k/driver"
 )
 
 // RemoveCluster remove all nodes
@@ -18,12 +23,46 @@ func (c *Command) RemoveCluster() error {
 		return err
 	}
 
-	// TODO: Kill Grid5000 job
+	// store already deleted jobs to minimize API calls
+	jobs := make(map[int]bool)
 
 	// remove hosts from libmachine storage
-	for _, v := range lst {
-		client.Remove(v.Name)
+	for _, h := range lst {
+		// only remove Grid5000 nodes
+		if h.DriverName == "g5k" {
+			driverConfig, err := getG5kDriverConfig(h.RawDriver)
+			if err != nil {
+				log.Errorf("Cannot remove node '%s' : %s", h.Name, err)
+			}
+
+			// check the job is already in the list of deleted jobs
+			if _, exist := jobs[driverConfig.G5kJobID]; !exist {
+				// send API call to kill job
+				c.api.KillJob(driverConfig.G5kJobID)
+
+				// add job ID to list of deleted jobs
+				jobs[driverConfig.G5kJobID] = true
+
+				log.Infof("Job '%v' killed", driverConfig.G5kJobID)
+			}
+
+			// remove node from libmachine storage
+			client.Remove(h.Name)
+
+			log.Infof("Node '%s' removed", h.Name)
+		}
 	}
 
 	return nil
+}
+
+// getG5kDriverConfig takes a raw driver and return a configured Driver structure
+func getG5kDriverConfig(rawDriver []byte) (*driver.Driver, error) {
+	// unmarshal driver configuration
+	var drv driver.Driver
+	if err := json.Unmarshal(rawDriver, &drv); err != nil {
+		return nil, err
+	}
+
+	return &drv, nil
 }
