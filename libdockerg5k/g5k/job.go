e1ce835a606b6b840371ec1d7db893c5d7cd3b33
gfieni <gfieni@users.noreply.github.com>
2017-03-13 16:48:14 +0100

    feat(create-cluster): Adding support for jobs walltime extension.

--- libdockerg5k/g5k/job.go
+++ libdockerg5k/g5k/job.go
@@ -8,16 +8,10 @@ import (
 
 // ReserveNodes allocate a new job with the required number of nodes on the given site, and returns the Job ID
 func (g *G5K) ReserveNodes(site string, nbNodes int, resourceProperties string, walltime string) (int, error) {
-	// convert walltime to seconds
-	seconds, err := api.ConvertDuration(walltime)
-	if err != nil {
-		return -1, err
-	}
-
 	// create a new job request with given parameters
 	jobReq := api.JobRequest{
 		Resources:  fmt.Sprintf("nodes=%v,walltime=%s", nbNodes, walltime),
-		Command:    fmt.Sprintf("sleep %v", seconds),
+		Command:    "sleep 365d",
 		Properties: resourceProperties,
 		Types:      []string{"deploy"},
 	}
