0c7d53fa0e21c03f47e9ac07e77f20a323cab0de
gfieni <gfieni@users.noreply.github.com>
2017-03-14 16:43:36 +0100

    refactor(libdockerg5k/swarm): Moving configuration to structures instead of function arguments

--- libdockerg5k/swarm/mode.go
+++ libdockerg5k/swarm/mode.go
@@ -1,28 +1,65 @@
 package swarm
 
-import "github.com/docker/machine/libmachine/host"
+import (
+	"fmt"
 
-// JoinMode define the join modes for Swarm
-type JoinMode int
-
-const (
-	// JoinAsManager makes the node join the cluster as Swarm Manager
-	JoinAsManager JoinMode = iota
-	// JoinAsWorker makes the node join the cluster as Swarm Worker
-	JoinAsWorker
+	"github.com/docker/machine/libmachine/host"
 )
 
-// InitSwarmModeCluster initialize a new Swarm mode cluster on the given host and returns the Manager/Worker join tokens
-func InitSwarmModeCluster(h *host.Host) (string, string, error) {
-	// Launch Weave Net Router
-	if _, err := h.RunSSHCommand("docker swarm init"); err != nil {
-		return "", "", err
-	}
-
-	return "", "", nil
+// SwarmModeGlobalConfig contain Swarm Mode global configuration
+type SwarmModeGlobalConfig struct {
+	ManagerToken        string
+	BootstrapManagerURL string
+	WorkerToken         string
 }
 
-// JoinSwarmModecluster makes the host join a Swarm mode cluster as Manager or Worker
-func JoinSwarmModecluster(host *host.Host, joinMode JoinMode, joinToken string) error {
+// IsSwarmModeClusterInitialized returns true if Swarm mode cluster is initialized (Manager/Worker tokens set), and false otherwise
+func (gc *SwarmModeGlobalConfig) IsSwarmModeClusterInitialized() bool {
+	return (gc.ManagerToken != "") || (gc.WorkerToken != "")
+}
+
+// InitSwarmModeCluster initialize a new Swarm mode cluster on the given host and returns the Manager/Worker join tokens
+func (gc *SwarmModeGlobalConfig) InitSwarmModeCluster(h *host.Host) error {
+	// check if Swarm mode cluster is already initialized
+	if gc.IsSwarmModeClusterInitialized() {
+		return fmt.Errorf("The Swarm Mode cluster is already initialized")
+	}
+
+	// init Swarm mode cluster
+	_, err := h.RunSSHCommand("docker swarm init")
+	if err != nil {
+		return err
+	}
+
+	// get Manager join token
+	gc.ManagerToken, err = h.RunSSHCommand("docker swarm join-token -q manager")
+	if err != nil {
+		return err
+	}
+
+	// get Worker join token
+	gc.WorkerToken, err = h.RunSSHCommand("docker swarm join-token -q worker")
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// JoinSwarmModeCluster makes the host join a Swarm mode cluster as Manager or Worker
+func (gc *SwarmModeGlobalConfig) JoinSwarmModeCluster(host *host.Host, isManager bool) error {
+	// by default, join as Worker
+	token := gc.WorkerToken
+
+	// if node is a manager, change the join token to Manager token
+	if isManager {
+		token = gc.ManagerToken
+	}
+
+	// run swarm join command
+	if _, err := host.RunSSHCommand(fmt.Sprintf("docker swarm join --token %s %s", token, gc.BootstrapManagerURL)); err != nil {
+		return err
+	}
+
 	return nil
 }
--- libdockerg5k/swarm/standalone.go
+++ libdockerg5k/swarm/standalone.go
@@ -1,9 +1,39 @@
 package swarm
 
-import "github.com/docker/swarm/discovery/token"
+import (
+	"github.com/docker/machine/libmachine/swarm"
+	"github.com/docker/swarm/discovery/token"
+)
 
-// GetNewSwarmDiscoveryToken get a new Docker Swarm discovery token from Docker Hub
-func GetNewSwarmDiscoveryToken() (string, error) {
+// SwarmStandaloneGlobalConfig contain Swarm standalone global configuration
+type SwarmStandaloneGlobalConfig struct {
+	Image          string
+	Discovery      string
+	Strategy       string
+	MasterFlags    []string
+	JoinFlags      []string
+	IsExperimental bool
+}
+
+// CreateSwarmStandaloneNodeConfig returns a configured SwarmOptions for HostOptions struct
+func (gc *SwarmStandaloneGlobalConfig) CreateSwarmStandaloneNodeConfig(nodeName string, isMaster bool, isWorker bool) *swarm.Options {
+	return &swarm.Options{
+		IsSwarm:            true,
+		Image:              gc.Image,
+		Agent:              isWorker,
+		Master:             isMaster,
+		Discovery:          gc.Discovery,
+		Address:            nodeName,
+		Host:               "tcp://0.0.0.0:3376",
+		Strategy:           gc.Strategy,
+		ArbitraryFlags:     gc.MasterFlags,
+		ArbitraryJoinFlags: gc.JoinFlags,
+		IsExperimental:     gc.IsExperimental,
+	}
+}
+
+// GetNewSwarmStandaloneDiscoveryToken get a new Docker Swarm discovery token from Docker Hub
+func GetNewSwarmStandaloneDiscoveryToken() (string, error) {
 	// init Discovery structure
 	discovery := token.Discovery{}
 	discovery.Initialize("token", 0, 0, nil)
